58a9ada7e60b28b76e822171d05a492cccc7623d
natadorosh <git.user.kras_red@mail.ru>
2025-05-19 00:48:27 +0300

    добавлена логика работы с числами

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма:");
+        int firstNumber = 5;
+        int secondNumber = 10;
+        double quotient = (double) firstNumber/secondNumber;
+        System.out.println(firstNumber+secondNumber);
+        System.out.println("Разница:");
+        System.out.println(firstNumber-secondNumber);
+        System.out.println("Произведение:");
+        System.out.println(firstNumber*secondNumber);
+        System.out.println("Частное:");
+        System.out.println(quotient);
     }
 }
